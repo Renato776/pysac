8a4c74615416e580295f04b84cbc8f16be0e6540
Renato Flores <renatojosuefloresperez@gmail.com>
2020-08-26 11:56:24 -0600

    Added way to create & save plotsgit add .

--- io2/inventarios.py
+++ io2/inventarios.py
@@ -1,9 +1,11 @@
 from base import LATEX_HEADER
-
-
+from sympy import plot
+from sympy.abc import x,y,z,n,t
 class Inventario:
     def __init__(self):
         self.someText = f'Hello imported text: {LATEX_HEADER}'
 
     def resumen(self):
+        graph = plot(x**2, show=False)
+        graph.save('./out/parabola.png')
         return 'I printed my own text {}.'.format(self.someText)
