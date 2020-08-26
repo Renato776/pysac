b14620189d5e786893aa85c07e5e88b83cda4e03
Renato Flores <renatojosuefloresperez@gmail.com>
2020-08-26 12:07:03 -0600

    Added requirements file

--- io2/inventarios.py
+++ io2/inventarios.py
@@ -1,11 +1,14 @@
 from base import LATEX_HEADER
 from sympy import plot
-from sympy.abc import x,y,z,n,t
+from sympy.abc import x, y, z, n, t
+
+
 class Inventario:
     def __init__(self):
         self.someText = f'Hello imported text: {LATEX_HEADER}'
 
     def resumen(self):
         graph = plot(x**2, show=False)
-        graph.save('./out/parabola.png')
+        fig = 'parabola-1'
+        graph.save(f'/home/renato/PycharmProjects/pysac/out/plots/{fig}.png')
         return 'I printed my own text {}.'.format(self.someText)
