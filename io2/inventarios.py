ec47148b4dd2096d90cfc3b0bd2e447dbe2a9bfd
Renato Flores <renatojosuefloresperez@gmail.com>
2020-08-30 18:54:11 -0600

    Added procedimiento!

--- io2/inventarios.py
+++ io2/inventarios.py
@@ -1,6 +1,16 @@
 from sympy import plot, symbols
 import sympy as ren
+import re
 
+def extract_chars(string):
+    chars = ''
+    nums = ''
+    for char in string:
+        if char.isnumeric():
+            nums += char
+        else:
+            chars += char
+    return chars, nums
 def p_section(title):
     return f'\\section{{{title}}}\n'
 def p_subsection(title):
@@ -15,14 +25,15 @@ def itemize(data):
 
 class Inventario:
     def print_data(self,args):
-        aux = p_subsection('Datos')
+        aux = p_subsection('\\Huge Datos')
         data = []
         for key in args:
             data.append(f'{key} = {args[key]}')
-        return aux + itemize(data)
+        return f'{aux}\\begin{{huge}}\n{itemize(data)}\\end{{huge}}'
     def __init__(self, name,**kwargs):
         self.content = p_section(name)
         self.values = kwargs
+        self.latex_symbols = {}
         self.targets = {'Q':'Cantidad Optima','S':'Cantidad Maxima en inventario',\
                         'C':'Costo','D':'Cantidad maxima de faltantes','t2':'tiempo 2',\
                         't1':'tiempo 1','t3':'tiempo 3','t4':'tiempo 4','Ct':'Costo total'}
@@ -46,6 +57,9 @@ class Inventario:
                         'D': symbols('D'),
                         'p': symbols('p'),
                         'Ct': symbols('Ct')}
+        for s in self.symbols:
+            aux = extract_chars(s)
+            self.latex_symbols[s] = s if aux[1] == '' else f'{aux[0]}_{{{aux[1]}}}'
         if 'k' in kwargs:
             if 'c2' in kwargs:
                 self.model = 1
@@ -53,7 +67,6 @@ class Inventario:
                                    (self.symbols['c1'] + self.symbols['c2'])) /
                                   (self.symbols['c1'] * self.symbols['c2'] * (
                                               1 - self.symbols['r'] / self.symbols['k'])))
-
                 self.S = ren.sqrt((2 * self.symbols['r'] * self.symbols['c2'] * self.symbols['c3']
                                    * (1 - self.symbols['r']/self.symbols['k']))/((self.symbols['c1']
                                   + self.symbols['c2'])*self.symbols['c1']))
@@ -114,11 +127,23 @@ class Inventario:
         for r in self.targets:
             if hasattr(self,r):
                 self.content += p_section(f'\\Huge {self.targets[r]} ({r})')
-                self.content +='\\begin{Huge}\n'
-                self.content += f'{r} = ${ren.latex(getattr(self,r))}$\\linebreak\\linebreak\n'
-                self.tuplas.append((r,getattr(self,r).subs(self.tuplas)))
-                self.content += f'{r} = {self.tuplas[len(self.tuplas)-1][1]}\n'
-                self.content += '\\end{Huge}\n'
+                self.content +='\\begin{huge}\n'
+                jimin = ren.latex(getattr(self,r))
+                self.content += f'${r} = {jimin}$\\rbreak\n'
+                kwargs[r] = getattr(self,r).subs(self.tuplas)
+                self.tuplas.append((r,kwargs[r]))
+                aux = jimin
+                for l in self.latex_symbols:
+                    ll = self.latex_symbols[l]
+                    if l not in kwargs:
+                        continue
+                    if ll.find('}') < 0:
+                        aux = re.sub(r"\b%s\b" % ll , f' ({kwargs[ll]}) ', aux)
+                    else:
+                        aux = aux.replace(ll,f'({kwargs[l]})')
+                self.content += f'${r} = {aux}$\\rbreak\n'
+                self.content += f'${r} = {round(float(self.tuplas[len(self.tuplas)-1][1]),3)}$\n'
+                self.content += '\\end{huge}\n'
 
     def resumen(self):
         return self.content
